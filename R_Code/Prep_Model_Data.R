480217c49e11902910d054bbf159de27ee3bdddb
Charl Marais <charl.marais@zestlife.co.za>
2016-03-17 15:45:07 +0200

    Updated, previous model was not really applicable for this problem

--- R_Code/Prep_Model_Data.R
+++ R_Code/Prep_Model_Data.R
@@ -1,38 +1,77 @@
 ####################################################################################################################
-# Preparing the model All_lap_Data #
-####################################
+# Preparing the model Data #
+############################
 
-DummyDat <- dummyVars(formula   =  "~.", 
-                      data      =  Dat_Loop, 
-                      fullRank  =  FALSE)
+DB_DAT$random <- runif(nrow(DB_DAT))
 
-Dat_Loop <- as.data.frame(predict(DummyDat, Dat_Loop))
+train <- DB_DAT[DB_DAT$random <= 0.7, ] 
+test  <- DB_DAT[DB_DAT$random >  0.7, ] 
 
-rm(DummyDat)
+train <- subset(train, select = -c(random, INCEPTIONDATE, CLIENTBIRTHDATE, CLIENTBANKNAME, CLIENTBANKACCOUNTTYPE, CLIENTBANKBRANCH))
+test  <- subset(test,  select = -c(random, INCEPTIONDATE, CLIENTBIRTHDATE, CLIENTBANKNAME, CLIENTBANKACCOUNTTYPE, CLIENTBANKBRANCH))
 
-outcomeName     <- "STATUS"
-predictorsNames <- names(Dat_Loop)[names(Dat_Loop) != outcomeName]
+response.names <- "STATUS"
 
-splitIndex <- createDataPartition(y      =  Dat_Loop[, outcomeName], 
-                                  p      =  0.75, 
-                                  list   =  FALSE, 
-                                  times  =  1)
+feature.names <- colnames(train)
+feature.names <- feature.names[feature.names != response.names]
 
-trainDF <- Dat_Loop[ splitIndex, ]
-testDF  <- Dat_Loop[-splitIndex, ]
-
-if (sum(trainDF$STATUS) == 0) {
-  
-  rowToAdd <- as.data.frame(testDF[testDF$STATUS == 1, ][1,])
-  trainDF  <- rbind(trainDF, rowToAdd)
-  
-  testDF  <- testDF[rownames(testDF) != rownames(testDF[testDF$STATUS == 1, ][1,]),]
-  
-  testDF  <- rbind(testDF, trainDF[1, ])
-  
-  trainDF <- trainDF[(2:nrow(trainDF)), ]
-  
+# put IDs for text variables - replace missings by 0 - boosting runs faster
+for (f in feature.names) {
+  if (class(train[[f]]) == "character") {
+    levels <- unique(c(train[[f]], test[[f]])) # Combines all possible factors (both from train and test)
+    train[[f]] <- factor(train[[f]], levels = levels)
+    test[[f]]  <- factor(test[[f]],  levels = levels)
+    # train[[f]] <- ifelse(is.na(train[[f]]), 0, as.integer(factor(train[[f]], levels = levels)))   # Create a numeric ID
+    # test[[f]]  <- ifelse(is.na(test[[f]]),  0, as.integer(factor(test[[f]],  levels = levels)))   # instead of a character ID (If NA then 0)
+  }
+  else if (class(train[[f]]) == "integer" | class(train[[f]]) == "numeric"){
+    train[[f]] <- ifelse(is.na(train[[f]]), 0, train[[f]])   # If NA then
+    test[[f]]  <- ifelse(is.na(test[[f]]),  0, test[[f]])    # 0
+  }
 }
+
+Outcome_tr <- train$STATUS
+train <- subset(train, select = -STATUS)
+Outcome_te <- test$STATUS
+test <- subset(test, select = -STATUS)
+
+Model <- gbm.fit(y                  =  Outcome_tr,                # Dependent variable
+                 x                  =  train,                     # Dataframe with features
+                 distribution       =  "bernoulli",               # Alternative for binary outcome : "adaboost"
+                 n.trees            =  5000,                      # Keep it large - then we prune afterwards
+                 shrinkage          =  0.01,                      # The smaller, the better - tradeoff = slower
+                 interaction.depth  =  3,                         # Use the results from cross validation to choose interaction depth
+                 n.minobsinnode     =  10,                        # Effect on overfitting - decreasing this parameter increases the in-sample fit 
+                                                                  #                       - can result in overfitting
+                 nTrain             =  round(nrow(train) * 0.8),  # To select the number of trees at the end 
+                 verbose            =  TRUE)                      # Print preliminary output
+
+# Look at the last model built
+# Relative influence among the variables can be used in variable selection
+summary(Model)
+
+# If you see one variable that's much more important than all of the rest, that could be evidence of overfitting
+
+# Optimal number of trees based on CV
+gbm.perf(Model)
+
+# Look at the effects of each variable, does it make sense?
+for(i in 1:length(Model$var.names)) {
+  plot(Model, 
+       i.var   =  i,
+       ntrees  =  gbm.perf(Model, plot.it = FALSE),
+       type    =  "response")
+}
+
+# Make predictions
+
+
+
+
+
+
+
+
 
 
 
