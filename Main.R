e9bfb59e685c1459ccc1100e9e86c570eb4d23f5
Charl Marais <charl.marais@zestlife.co.za>
2016-04-05 15:14:58 +0200

    Some important (but small) updates

--- Main.R
+++ Main.R
@@ -27,11 +27,27 @@ dbDisconnectAll()
 # Cleans Data #
 ###############
 
+# If offline data is needed
+load(paste(getwd(), "/Offline_WS.RData", sep = ""))
+
 # Cleans data used for modelling
 source(paste(Path, "/R_Code/Clean_Model_Data.R", sep = ""))
 
 # Cleans data used for prediction
 source(paste(Path, "/R_Code/Append_Sort.R", sep = "")) # To be adjusted - loads of changes
+
+#####################################################################################################
+# Allocation #
+##############
+
+# Load Current model and start allocation script
+load(paste(getwd(), "/Active_Model.RData", sep = ""))
+
+repeat {
+  
+  source(paste(Path, "/R_Code/Allocation.R", sep = ""))
+  
+}
 
 #####################################################################################################
 # Prepare Model Data #
@@ -45,14 +61,21 @@ source(paste(Path, "/R_Code/Prep_Model_Data.R", sep = ""))
 
 source(paste(Path, "/R_Code/Build_Model.R", sep = ""))
 
+# Updates Model
+
+source(paste(Path, "/R_Code/Model_Updater.R", sep = ""))
+
 #####################################################################################################
 # Test Model #
 ##############
 
 source(paste(Path, "/R_Code/Testing.R", sep = ""))
 
-impdf
-print(auc$auc)
+#####################################################################################################
+# Analyse Model #
+#################
+
+source(paste(Path, "/R_Code/Model_Analysis.R", sep = ""))
 
 
 
