61ad8c13af2943bb7a8cbdde29a3650db8974772
Charl Marais <charl.marais@zestlife.co.za>
2016-04-22 11:37:51 +0200

    Minor updates

--- R_Code/Testing.R
+++ R_Code/Testing.R
@@ -6,25 +6,14 @@ TestPred <- predict(object = Model,
                     n.trees = gbm.perf(Model),
                     type = "response")
 
-AUC::auc(AUC::roc(TestPred, as.factor(Outcome_te))) # 0.7266963
+AUC::auc(AUC::roc(TestPred, as.factor(test$STATUS))) # 0.7446556
 
 TrainPred <- predict(object = Model,
                      newdata = train,
                      n.trees = gbm.perf(Model),
                      type = "response")
 
-AUC::auc(AUC::roc(TrainPred, as.factor(Outcome_tr))) # 0.7339922
-
-# Adding original Predicted values and finding optimal allocations
-
-train$Pred <- TrainPred
-test$Pred <- TestPred
-
-TotPred <- rbind(subset(test, select = -ID), train)
-
-ByAgent <- TotPred %>% 
-  group_by(ZLAGENT) %>%
-  top_n(1, wt = Pred)
+AUC::auc(AUC::roc(TrainPred, as.factor(train$STATUS))) # 0.755016
 
 #   -----------------------------------------------------------------------
 
