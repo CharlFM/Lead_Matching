a0ff709ae5df07b1735db01a24be46cea93a190b
Charl Marais <charl.marais@zestlife.co.za>
2016-04-22 11:34:42 +0200

    Start

--- R_Code/Allocation.R
+++ R_Code/Allocation.R
@@ -1,1 +1,8 @@
+#####################################################################################################
+# Cleans Data #
+###############
+
+# Cleans data used for prediction
+source(paste(Path, "/R_Code/Append_Sort.R", sep = "")) # To be adjusted - loads of changes
+
 #
