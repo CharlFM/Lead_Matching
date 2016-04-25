2d7f218998dc1bcb27bb9c5b9c27c82ce5aad210
Charl Marais <charl.marais@zestlife.co.za>
2016-04-25 15:25:47 +0200

    Small update

--- Main.R
+++ Main.R
@@ -14,22 +14,23 @@ Path <- getwd()
 
 source(paste(Path, "/R_Code/Initialize.R", sep = ""))
 
-# Enter "YES" or "NO", this will determine if offline data should be used (faster but outdated data)
-Load_Offline_Data <- "YES"
-
 #####################################################################################################
 # Modelling #
 #############
 
+# Enter "YES" or "NO", this will determine if offline data should be used (faster but outdated data)
+Load_Offline_Data <- "YES"
+
 # Important to step through the insides of this script when updating the model.
-source(paste(Path, "/R_Code/Modelling_Section.R", sep = "")) 
+source(paste(Path, "/R_Code/Modelling_Section.R", sep = ""))  # Need to take out parts that is done in the data import step - such as appending Culture and Race info where it already exists (if in new db, old db - update)
 
 #####################################################################################################
 # Allocation #
 ##############
 
 # Loads New lead data - For Allocation
-source(paste(Path, "/R_Code/Load_Lead_Data.R", sep = "")) # TO BE MOVED TO INSIDE THE LOOP !!!! Will be a seperate script that will run from VB.NET
+source(paste(Path, "/R_Code/Load_Lead_Data.R", sep = "")) # TO BE MOVED TO INSIDE THE LOOP (continuously read new lead data) !!!! 
+                                                          # Will be a seperate script that will run from VB.NET
 
 # Load Current model and start allocation script
 load(paste(getwd(), "/Active_Model.RData", sep = ""))
