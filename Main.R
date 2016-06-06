45d2dd0dfa909e35ec92d1be148054fd9870baa1
Charl Marais <charl.marais@zestlife.co.za>
2016-06-06 12:51:31 +0200

    New Lead allocation will be in it's own section

--- Main.R
+++ Main.R
@@ -28,9 +28,6 @@ source(paste(Path, "/R_Code/Modelling_Section.R", sep = ""))  # Need to take out
 # Allocation #
 ##############
 
-# Loads New lead data to DB - For Allocation
-source(paste(Path, "/R_Code/Load_Lead_Data.R", sep = "")) # TO BE MOVED TO INSIDE THE LOOP (continuously read new lead data) !!!! 
-                                                          # Will be a seperate script that will run from VB.NET
 repeat {
   
   source(paste(Path, "/R_Code/Allocation.R", sep = ""))
