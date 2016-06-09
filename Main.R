8d1c9d9611dc5bf629b31b2e1c8b9d36c8bdaede
Charl Marais <charl.marais@zestlife.co.za>
2016-06-09 08:00:42 +0200

    Automation moved to new file

--- Main.R
+++ Main.R
@@ -34,54 +34,7 @@ source(paste(Path, "/R_Code/Modelling_Section.R", sep = ""))  # Need to take out
 source(paste(Path, "/R_Code/Allocation_Manual_New.R", sep = ""))
 
 # Run from here
-done_today <- 0
-repeat {
-  
-  # Disable after 10PM and before 5AM
-  curtime <- as.numeric(format(Sys.time(), "%H")) + as.numeric(format(Sys.time(), "%M"))/60
-  
-  if (curtime < 5) {
-    done_today <- 0
-  }
-  
-  if (curtime > 5 & curtime < 22) {
-    
-    rm(list = ls())
-    gc()
-    
-    Path <- getwd()
-    
-    source(paste(Path, "/R_Code/Initialize.R", sep = ""))
-    
-    curday  <- weekdays(Sys.Date())
-    curtime <- as.numeric(format(Sys.time(), "%H")) + as.numeric(format(Sys.time(), "%M"))/60
-    
-    # Do recycling every Wednesday between 13:00 and 14:00 (thus curtime between 13.00 and 14.00)
-    # Also do recycling every Thursday and Friday between 5AM and 6AM
-    if (done_today == 0 & ((curday == "Wednesday" & curtime > 13 & curtime < 14) |
-        (curday == "Thursday" & curtime > 5 | curtime < 6) |
-        (curday == "Friday" & curtime > 5 | curtime < 6))) {
-      
-      source(paste(Path, "/R_Code/Allocation_Manual_Recycled.R", sep = ""))
-      done_today <- 1
-      
-      print(paste("Recycle at", Sys.time()))
-      
-    } else if ((curtime - floor(curtime))  > 0.4 | (curtime - floor(curtime)) < 0.6) { # Recalc probs in DB each half hour
-
-    } else if ((curtime - floor(curtime))  > 0.4 | (curtime - floor(curtime)) < 0.6) { # Recalc probabilities in DB each half an hour
-      
-      source(paste(Path, "/R_Code/Recalc_DB_Probs.R", sep = ""))
-      
-      print(paste("Recalc at", Sys.time()))
-      
-    }
-    
-    dbDisconnectAll()
-    
-  }
-  
-}
+source(paste(Path, "/R_Code/DataUpdater.R", sep = ""))
 
 
 
