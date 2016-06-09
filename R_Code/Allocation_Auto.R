22ee26f28badf4b8f8e50d464471f920ae8960e3
Charl Marais <charl.marais@zestlife.co.za>
2016-06-09 23:00:22 +0200

    Fix

--- R_Code/Allocation_Auto.R
+++ R_Code/Allocation_Auto.R
@@ -10,37 +10,41 @@ LeadOut <- data.frame(ZLAGENT = as.character(),
                       ID      = as.integer())
 
 # Allocate/Rank Owned Affinities ------------------------------------------
-for (aff in OwnedAffs) {
+if (nrow(OwnedAffs) > 0) {
   
-  agent   <- Allocation_Dat$ZLAGENT[gsub(" ", "", gsub("[^[:alpha:] ]", "", toupper(Allocation_Dat$Owner))) == aff]
-  agentid <- Allocation_Dat$AgentID[gsub(" ", "", gsub("[^[:alpha:] ]", "", toupper(Allocation_Dat$Owner))) == aff]
-  
-  SubsetData <- ManLead_Dat[grepl(aff, ManLead_Dat$AFFINITY), ]
-  SubsetData <- SubsetData[, c(Model$var.names, "ID")]
-  
-  if (nrow(SubsetData) > 0) {
+  for (aff in OwnedAffs) {
     
-    SubsetData$ZLAGENT <- agent
-    SubsetData$ZLAGENT <- as.factor(SubsetData$ZLAGENT)
+    agent   <- Allocation_Dat$ZLAGENT[gsub(" ", "", gsub("[^[:alpha:] ]", "", toupper(Allocation_Dat$Owner))) == aff]
+    agentid <- Allocation_Dat$AgentID[gsub(" ", "", gsub("[^[:alpha:] ]", "", toupper(Allocation_Dat$Owner))) == aff]
     
-    Preds <- predict(object = Model,
-                     newdata = SubsetData,
-                     n.trees = ntrees,
-                     type = "response")
-    SubsetData$Pred <- Preds
+    SubsetData <- ManLead_Dat[grepl(aff, ManLead_Dat$AFFINITY), ]
+    SubsetData <- SubsetData[, c(Model$var.names, "ID")]
     
-    UpdateR <- data.frame(ZLAGENT = Allocation_Dat$ZWINGMASTER[Allocation_Dat$AgentID == agentid],
-                          Pred    = SubsetData$Pred, 
-                          ID      = SubsetData$ID) 
-    
-    LeadOut <- rbind(LeadOut, UpdateR)
-    
-    source(paste(Path, "/R_Code/Insert.R", sep = ""))
-    
-    ManLead_Dat     <- ManLead_Dat[!(ManLead_Dat$ID %in% SubsetData$ID), ]
-    ManLead_DatOrig <- ManLead_DatOrig[!(ManLead_DatOrig$ID %in% SubsetData$ID), ]
+    if (nrow(SubsetData) > 0) {
+      
+      SubsetData$ZLAGENT <- agent
+      SubsetData$ZLAGENT <- as.factor(SubsetData$ZLAGENT)
+      
+      Preds <- predict(object = Model,
+                       newdata = SubsetData,
+                       n.trees = ntrees,
+                       type = "response")
+      SubsetData$Pred <- Preds
+      
+      UpdateR <- data.frame(ZLAGENT = Allocation_Dat$ZWINGMASTER[Allocation_Dat$AgentID == agentid],
+                            Pred    = SubsetData$Pred, 
+                            ID      = SubsetData$ID) 
+      
+      LeadOut <- rbind(LeadOut, UpdateR)
+      
+      ManLead_Dat     <- ManLead_Dat[!(ManLead_Dat$ID %in% SubsetData$ID), ]
+      ManLead_DatOrig <- ManLead_DatOrig[!(ManLead_DatOrig$ID %in% SubsetData$ID), ]
+      
+    }
     
   }
+  
+  source(paste(Path, "/R_Code/Insert.R", sep = ""))
   
 }
 
